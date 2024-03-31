38e501b12ac48125df0bce167de815889bf0f6b8
Judah Lit <Judahlit03@gmail.com>
2024-04-01 00:18:19 +0200

    Created Spring Boot app with Docker files

---
+++ src/main/java/com/wavshare/OwnershipService/OwnershipServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wavshare.OwnershipService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnershipServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnershipServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wavshare/OwnershipService/OwnershipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wavshare.OwnershipService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnershipServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
